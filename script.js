4d8ea76397876c88798f21bbe7774eaa06ae0158
SandunLB <sandunlb2001@gmail.com>
2024-08-02 23:35:58 +0530

    commit #3

--- script.js
+++ script.js
@@ -33,3 +33,13 @@ const init = function () {
   player0El.classList.add('player--active');
   player1El.classList.remove('player--active');
 };
+
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
